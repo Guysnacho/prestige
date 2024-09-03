9560c23ec5b80428be0eba87a58c83dfe9358566
Samuel Adetunji <guysnacho2@gmail.com>
2024-09-03 01:27:29 -0500

    Add tanstack query

--- client/packages/app/provider/index.tsx
+++ client/packages/app/provider/index.tsx
@@ -1,26 +1,34 @@
-import { useColorScheme } from 'react-native'
 import { CustomToast, TamaguiProvider, TamaguiProviderProps, ToastProvider, config } from '@my/ui'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useColorScheme } from 'react-native'
 import { ToastViewport } from './ToastViewport'
 
 export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
   const colorScheme = useColorScheme()
-  
+  const queryClient = new QueryClient()
+
   return (
-    <TamaguiProvider config={config} defaultTheme={colorScheme === 'dark' ? 'dark' : 'light'} {...rest}>
-      <ToastProvider
-        swipeDirection="horizontal"
-        duration={6000}
-        native={
-          [
-            /* uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go */
-            // 'mobile'
-          ]
-        }
+    <QueryClientProvider client={queryClient}>
+      <TamaguiProvider
+        config={config}
+        defaultTheme={colorScheme === 'dark' ? 'dark' : 'light'}
+        {...rest}
       >
-        {children}
-        <CustomToast />
-        <ToastViewport />
-      </ToastProvider>
-    </TamaguiProvider>
+        <ToastProvider
+          swipeDirection="horizontal"
+          duration={6000}
+          native={
+            [
+              /* uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go */
+              // 'mobile'
+            ]
+          }
+        >
+          {children}
+          <CustomToast />
+          <ToastViewport />
+        </ToastProvider>
+      </TamaguiProvider>
+    </QueryClientProvider>
   )
 }
