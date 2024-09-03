c696ef987edd4ea5d0f8bcf3b340283b442fedc3
Samuel Adetunji <guysnacho2@gmail.com>
2024-09-03 02:06:18 -0500

    Add query but cors is kicking us out

--- client/packages/app/features/user/driver-screen.tsx
+++ client/packages/app/features/user/driver-screen.tsx
@@ -1,9 +1,35 @@
-import { Button, Paragraph, YStack } from '@my/ui'
+import { Button, Paragraph, Spinner, YStack, useToastController } from '@my/ui'
 import { Car, ChevronLeft } from '@tamagui/lucide-icons'
-import { useParams, useRouter } from 'solito/navigation'
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
+import { useRouter } from 'solito/navigation'
 
 export function DriverHomeScreen() {
   const router = useRouter()
+  const toast = useToastController()
+  const [user, setUser] = useState('')
+  const [isPolling, setIsPolling] = useState(false)
+
+  const { mutate, data, isIdle, isPending } = useMutation({
+    mutationFn: async () =>
+      fetch(`${process.env.NEXT_PUBLIC_SERVER_HOST}/driver/trip`, {
+        method: 'POST',
+        referrer: process.env.NEXT_PUBLIC_SERVER_HOST,
+        referrerPolicy: 'origin',
+        body: JSON.stringify({ name: user }),
+      }),
+    onError(err, v, c) {
+      console.error(err)
+      toast.show('Issue while joining pool', { message: err?.message })
+    },
+    async onSuccess(data, v, c) {
+      var res = await data.json()
+      toast.show('Successfully joined pool', {
+        message: `${res.message}\n${res.requestId}`,
+      })
+    },
+  })
+
   function joinDriverPool() {
     console.error('Function not implemented.')
   }
@@ -15,7 +41,12 @@ export function DriverHomeScreen() {
       <Paragraph ta="center" fow="700" col="$blue10">
         {`Driver : ` + '${id}'}
       </Paragraph>
-      <Button iconAfter={Car} onPress={() => joinDriverPool()}>
+      <Button
+        iconAfter={isPending ? Spinner : Car}
+        variant={isPending ? 'outlined' : undefined}
+        disabled={isPending}
+        onPress={() => mutate()}
+      >
         Join Pool
       </Button>
       <Button icon={ChevronLeft} onPress={() => router.back()}>
