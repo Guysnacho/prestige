4c649e78b92e571fb85543da2a3b48753c0a0b17
Samuel Adetunji <guysnacho2@gmail.com>
2024-09-09 01:52:08 -0500

    Add supabase and check if authed before routing to driver/rider pages

--- client/apps/next/pages/index.tsx
+++ client/apps/next/pages/index.tsx
@@ -1,5 +1,27 @@
+import { AuthScreen } from '@my/app/features/auth/auth-screen'
+import { createClient } from '@my/app/util/server-props'
+import { User } from '@supabase/supabase-js'
 import { HomeScreen } from 'app/features/home/screen'
+import { GetServerSidePropsContext } from 'next/types'
+import { useState } from 'react'
 
-export default function Page() {
-  return <HomeScreen />
+type HomeProps = {
+  user?: User
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // Fetch data from external API
+  const client = createClient(context)
+  const { data, error } = await client.auth.getUser()
+  if (error) {
+    return { props: { user: null } }
+  } else {
+    return { props: { user: data.user } }
+  }
+}
+
+export default function Page({ user }: HomeProps) {
+  const [signUp, setSignUp] = useState(false)
+
+  return user ? <HomeScreen /> : <AuthScreen signUp={signUp} setSignUp={setSignUp} />
 }
