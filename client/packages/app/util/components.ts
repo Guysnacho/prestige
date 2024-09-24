bbb5aff349e9ff67c88035bd3f6d4e6d8d14dba7
Samuel Adetunji <guysnacho2@gmail.com>
2024-09-23 22:25:42 -0500

    Single supa client on component

--- client/packages/app/util/components.ts
+++ client/packages/app/util/components.ts
@@ -4,7 +4,10 @@ import { Database } from './schema'
 export function createClient() {
   const supabase = createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      isSingleton: true,
+    }
   )
 
   return supabase
