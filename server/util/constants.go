6c8616fa71f38789126d35e21263d6a235a38cb6
Samuel Adetunji <guysnacho2@gmail.com>
2024-09-25 21:24:17 -0500

    Add type that I don't use

--- server/util/constants.go
+++ server/util/constants.go
@@ -9,4 +9,18 @@ type Location struct {
 	Lat string `json:"lat" binding:"required"`
 }
 
+type Trip struct {
+	Id          int     `json:"id" binding required`
+	Driver      *string `json:"driver"`
+	Rider       string  `json:"rider" binding required`
+	Status      string  `json:"status" binding required`
+	Pickup_time string  `json:"pickup_time" binding required`
+	Pickup_lng  float64 `json:"pickup_lng" binding required`
+	Pickup_lat  float64 `json:"pickup_lat" binding required`
+	Dest_lng    float64 `json:"dest_lng" binding required`
+	Dest_lat    float64 `json:"dest_lat" binding required`
+	Created_at  string  `json:"created_at" binding required`
+	Updated_at  string  `json:"updated_at" binding required`
+}
+
 const UserNotFound = "user not found"
