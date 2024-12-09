d8939dac849ee8ae0e1d0abd789891a99c1f8338
Samuel Adetunji <guysnacho2@gmail.com>
2024-12-08 19:07:07 -0600

    Add router store

---
+++ client/packages/app/store/routerStore.ts
@@ -0,0 +1,28 @@
+import { RadarAddress } from 'react-native-radar'
+import { create } from 'zustand'
+
+export interface RouteState {
+  pickup: RadarAddress | undefined
+  destination: RadarAddress | undefined
+  pickupTime: Date | null
+  setPickup: (addy: RadarAddress) => void
+  setDestination: (addy: RadarAddress) => void
+  setPickupTime: (pickupTime: Date) => void
+}
+
+/**
+ * Navigation Routing State
+ *
+ * @example
+ * // This is how we use the user store
+ * const store = useStore(useUserStore, (store) => store);
+ */
+export const useRouterStore = create<RouteState>((set, get) => ({
+  pickupTime: null,
+  pickup: undefined,
+  destination: undefined,
+  setPickupTime: (pickupTime) => set({ pickupTime }),
+  setPickup: (pickup) => set({ pickup }),
+  setDestination: (destination) => set({ destination }),
+  clear: () => set({ pickupTime: null, pickup: undefined, destination: undefined }),
+}))
